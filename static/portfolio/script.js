7b84bc9ce1dce4877abf6fea506caf462df8f6be
g-adamante <gabriel.henrique.a@gmail.com>
2018-09-21 17:58:21 +0200

    portfolio

--- static/portfolio/script.js
+++ static/portfolio/script.js
@@ -15,3 +15,18 @@ function doAnimation(id, animName, duration, delay) {
 }
 
 doAnimation('conversar', 'shake', 1000, 2500);
+
+var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+      [100, 300, 600, 900, 1200, 1600, 1900]
+    ]
+  };
+  
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object.
+  var chart = new Chartist.Line('.ct-chart', data);
+  
